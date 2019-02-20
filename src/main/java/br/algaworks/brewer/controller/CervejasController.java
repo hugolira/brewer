2e30a33d129f2b1319903e0896dcaeba664ea682
hugolira <hugolira0@hotmail.com>
2019-02-20 00:17:39 -0300

    segundo commit

--- src/main/java/br/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/br/algaworks/brewer/controller/CervejasController.java
@@ -1,8 +1,15 @@
 package br.algaworks.brewer.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CervejasController {
-
+	
+	@RequestMapping("/cervejas/novo")
+	public String novo() {
+		return "cerveja/CadastroCerveja";
+	}
+	
+	
 }
